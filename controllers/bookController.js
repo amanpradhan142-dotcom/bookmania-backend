3b2e6c7659fbdd14acb810f6cc9e58acb70d654e
aman <amanpradhan142@gmail.com>
2026-06-07 13:08:21 +0530

    Fixed image and cart issues

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,28 +1,49 @@
 const Book = require('../models/Book');
 
-// Get all books
 const getBooks = async (req, res) => {
-    try {
-        const books = await Book.find();
-        res.json(books);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-// Add new book
 const addBook = async (req, res) => {
-    try {
-        const book = new Book(req.body);
-        const savedBook = await book.save();
+  try {
+    const book = new Book(req.body);
+    const savedBook = await book.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-        res.status(201).json(savedBook);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+const deleteBook = async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+
+    if (!deletedBook) {
+      return res.status(404).json({
+        success: false,
+        message: 'Book not found',
+      });
     }
+
+    res.json({
+      success: true,
+      message: 'Book deleted successfully',
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 module.exports = {
-    getBooks,
-    addBook,
+  getBooks,
+  addBook,
+  deleteBook,
 };
--- routes/BookRoutes.js
+++ routes/BookRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {
     getBooks,
     addBook,
+    deleteBook,
 } = require('../controllers/bookController');
 
 const router = express.Router();
@@ -10,5 +11,7 @@ const router = express.Router();
 router.get('/', getBooks);
 
 router.post('/', addBook);
+
+router.delete('/:id', deleteBook);
 
 module.exports = router;
